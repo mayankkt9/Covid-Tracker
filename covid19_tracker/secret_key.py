f2cc2bdeeb65d16e49fb8a3365b256ba1a80e23c
Mayank Kataruka <mayankkataruka@Mayanks-MacBook-Pro.local>
2020-06-24 14:52:50 -0700

    settings django

---
+++ covid19_tracker/secret_key.py
@@ -0,0 +1,6 @@
+# This is the secret key used by your application.
+# It should NEVER get in other hands, as it is the 
+# key to securing signed data - if you do not 
+# keep this secure, attackers could use it to generate 
+# their own signed values.
+key = 'k$$%%$q(*6%7d7r@4bok1(fvp-363jpu8n(1iv#3z2%maow*s%'
