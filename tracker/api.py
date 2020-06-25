5a3f0d0968216d368cb3c09d4f125155e89e86ea
Mayank Kataruka <mkataruk@asu.edu>
2020-06-25 00:01:42 -0700

    Adding more functionality to get infected case count

--- tracker/api.py
+++ tracker/api.py
@@ -52,6 +52,7 @@ class API:
     def get_current_number(self):
         """
         Scrapes the latest number of confirmed cases, deaths and recoveries from BNO's spreadsheet and returns them as ints
+
         Returns:
             (int) total - total confirmed cases
             (int) deaths - total deaths
@@ -66,14 +67,125 @@ class API:
         return int(total), int(deaths), int(recov)
 
     
+    def get_live(self):
+        """
+         Gets number of infections, deaths, recovered patients, active cases, serious cases, mortality, new cases and new deaths for every country.
+
+        Returns:
+            (List<Data_Object>) A list of objects which contain the information 
+        """
+        url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR30F8lYP3jG7YOq8es0PBpJIE5yvRVZffOyaqC0GgMBN6yt0Q-NI8pxS7hd1F9dYXnowSC6zpZmW9D/pubhtml/sheet?headers=false&gid=0&range=A7:I210"
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, 'html.parser')
+        trs = soup.findAll("tr")[1:]
+        data_list = []
+
+        for tr in trs:
+            tds = tr.findAll("td")[:-1] # the last td contains the source link
+            data_list.append(Data_Object(tds[0].text, tds[1].text.replace(",",""), tds[2].text.replace(",",""), tds[3].text.replace(",",""), tds[4].text.replace(",",""), tds[6].text.replace(",",""), tds[7].text.replace(",","")))
+        inf, deaths, rec = self.get_current_number()
+
+
+        url_glob = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR30F8lYP3jG7YOq8es0PBpJIE5yvRVZffOyaqC0GgMBN6yt0Q-NI8pxS7hd1F9dYXnowSC6zpZmW9D/pubhtml/sheet?headers=false&gid=0&range=A212:I212"
+        req = requests.get(url_glob)
+        soup = BeautifulSoup(req.text, 'html.parser')
+        tr = soup.findAll("tr")[1:]
+
+        tds = tr[0].findAll("td") # the last td contains the source link
+        try:
+            n_c = tds[2].text.replace(",","")
+            n_d = tds[4].text.replace(",","")
+            ser = tds[6].text.replace(",","")
+        except Exception as e:
+            n_c = 0
+            n_d = 0
+            ser = 0
+
+        if n_c == "" or n_d == "" or ser == "":
+            n_c = 0
+            n_d = 0
+            ser = 0
+
+        global_ = Data_Object("Global", inf, n_c, deaths, n_d, ser, rec)
+        data_list.append(global_)
+
+        return data_list
+
+
+    def get_latest(self):
+        """
+        Gets yesterday's confirmed cases and deaths from the JHU repository
+
+        Parameters:
+            (Dict) data_dict - {"country":"{"date":[cases, deaths]}, ...}"
+        """
+        url_confirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+        url_deaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+        #url_recovered = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+
+        data_dict = {}
+        prov_dict = {}
+
+        # Fetching the data from the Johns Hopkins CSSE's repository
+        conf = pd.read_csv(url_confirmed, error_bad_lines=False)
+        deaths = pd.read_csv(url_deaths, error_bad_lines=False)
+        #recov = pd.read_csv(url_recovered, error_bad_lines=False)
+
+        # We do not need Latitude / Logitude information, so we'll just delete these columns
+        del conf['Lat'], conf['Long'], deaths['Lat'], deaths['Long']#, recov['Lat'], recov['Long']
+        
+        # Making a list containing the last date in the .csv
+        cols = list(conf.columns.values)[-1:]
+        inf = 0
+        d = 0
+        rec = 0 
+
+        # Building the dict
+        for index, row in conf.iterrows():
+            data_deaths = deaths.iloc[index]
+            #data_rec = recov.loc[index]
+            
+            if not row[1] in data_dict:
+                data_dict[row[1]] = {}
+                data_dict[row[1]][cols[0]] = [row[cols[0]], data_deaths[cols[0]]]# data_rec[cols[0]], data_deaths[cols[0]]]
+            elif row[0] in ['French Guiana', 'Guadeloupe', 'Guam', 'Mayotte', 'occupied Palestinian territory', 'Puerto Rico', 'Reunion']:
+                # the states in the list above are listed as territories in the .csv's, however they are countries at the same time,
+                # so we'll need to make an exception
+                data_dict[row[0]] = {}
+                data_dict[row[0]][cols[0]] = [row[cols[0]], data_deaths[cols[0]]]# data_rec[cols[0]], data_deaths[cols[0]]]
+            else:
+                data_dict[row[1]][cols[0]] = [x + y for x, y in zip(data_dict[row[1]][cols[0]], [row[cols[0]], data_deaths[cols[0]]])]# data_rec[cols[0]], data_deaths[cols[0]]])]
+     
+        return data_dict
+
+
+
+    def get_all(self):
+        """
+        Gets timeseries of infections, deaths and recovered patients for every country.
+
+        Returns:
+            (Dict) A dict: {"country":"{"date":[cases, deaths]}, ...}"
+        """
+        url_confirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+        url_deaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+        url_recovered = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
+        
+        self.countries_dict = self.get_infected_countries(url_confirmed, url_deaths, url_recovered)
+        
+        return self.countries_dict
+
+
     def get_infected_countries(self, url_confirmed, url_deaths, url_recovered):
         """ Queries the Johns Hopkins CSSE's repository for all infected countries 
             and builds a dict with confirmed cases, deaths and recoveries for every
             country.
+
             Arguments:
             -- url_confirmed: (String) Url pointing to the .csv containing the numbers of confirmed cases
             -- url_deaths: (String) Url pointing to the .csv containing the numbers of deaths
             -- url_recovered: (String) Url pointing to the .csv containing the numbers of recovered cases
+
             Returns: 
                 (Dict) A dict with the format {"country":"{"date":[cases, recoveries, deaths]}, ...}"
                
